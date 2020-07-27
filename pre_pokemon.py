848e9ec85b0177b7aea1378e692bd681ed111bca
vincentchen1 <67167853+vincentchen1@users.noreply.github.com>
2020-07-27 10:36:40 -0700

    Update pre_pokemon.py

--- pre_pokemon.py
+++ pre_pokemon.py
@@ -11,18 +11,18 @@ class Pokemon:
     #     self.level = input("What is your Pokemon's level? ")
 
     def __init__(self, _name, _type, _level):
-
         self.name = _name
         self.type = _type
         self.level = _level
 
 
 class Trainer:
-    def __init__(self, _trainer_first_name, _trainer_last_name, _trainer_age, _trainer_ethnicity):
+    def __init__(self, _trainer_first_name, _trainer_last_name, _trainer_age, _trainer_ethnicity, _trainer_pokemon):
         self.first_name = _trainer_first_name
         self.last_name = _trainer_last_name
         self.age = _trainer_age
         self.ethnicity = _trainer_ethnicity
+        self.pokemon = _trainer_pokemon
 
 
 # subclass JSONEncoder
@@ -37,27 +37,55 @@ class TrainerEncoder(JSONEncoder):
 
 
 out_file = open('pokemon.json', 'w')
+data = {}
+
+
+add_trainer = input("Do you want to add a trainer? (yes/no) ")
+if add_trainer == 'no':
+    quit()
+else:
+    input_trainer_first_name = input("What is your first name? ")
+    input_trainer_last_name = input("What is your last name? ")
+    input_trainer_age = input("What is your age? ")
+    input_trainer_ethnicity = input("What is your ethnicity? ")
+    _trainer_pokemon = []
+    trainer = Trainer(input_trainer_first_name, input_trainer_first_name, input_trainer_age, input_trainer_ethnicity,
+                      _trainer_pokemon)
+    print(TrainerEncoder().encode(trainer))
+#    trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
+#    json.dump(trainer, out_file, cls=TrainerEncoder)
+    print()
 
 
 while True:
-    start = input("Do you want to add a trainer? yes/no ")
-    if start == 'yes':
-        input_trainer_first_name = input("What is your first name? ")
-        input_trainer_last_name = input("What is your last name? ")
-        input_trainer_age = input("What is your age? ")
-        input_trainer_ethnicity = input("What is your ethnicity? ")
-        trainer = Trainer(input_trainer_first_name, input_trainer_first_name, input_trainer_age, input_trainer_ethnicity)
-        print(TrainerEncoder().encode(trainer))
-        trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
-        json.dump(trainer, out_file, cls=TrainerEncoder)
-        print( )
+    add_pokemon = input("Do you want to add a Pokemon? (yes/no) ")
+    if add_pokemon == 'no':
+        add_trainer = input("Do you want to add a trainer? (yes/no) ")
+        if add_trainer == 'no':
+            break
+        else:
+            input_trainer_first_name = input("What is your first name? ")
+            input_trainer_last_name = input("What is your last name? ")
+            input_trainer_age = input("What is your age? ")
+            input_trainer_ethnicity = input("What is your ethnicity? ")
+            _trainer_pokemon = []
+            trainer = Trainer(input_trainer_first_name, input_trainer_first_name, input_trainer_age,
+                              input_trainer_ethnicity, _trainer_pokemon)
+            print(TrainerEncoder().encode(trainer))
+            trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
+            json.dump(trainer, out_file, cls=TrainerEncoder)
+            print()
+
+    else:
         input_name = input("What is your Pokemon's name? ")
         input_type = input("What is your Pokemon's type? ")
         input_level = input("What is your Pokemon's level? ")
         pokemon = Pokemon(input_name, input_type, input_level)
         print(PokemonEncoder().encode(pokemon))
-        pokemonJsonStr = json.dumps(pokemon, indent=6, cls=PokemonEncoder)
-        json.dump(pokemon, out_file, cls=PokemonEncoder)
-        print()
-    else:
-        quit()
+        _trainer_pokemon = []
+        trainer = Trainer(input_trainer_first_name, input_trainer_last_name, input_trainer_age, input_trainer_ethnicity,
+                          _trainer_pokemon)
+        _trainer_pokemon.append(pokemon)
+        print(TrainerEncoder().encode(trainer))
+        trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
+        json.dump(trainer, out_file, cls=TrainerEncoder)
