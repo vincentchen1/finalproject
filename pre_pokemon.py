7e6af586605ff40a3ee565d9be09c0855c37c528
vincentchen1 <67167853+vincentchen1@users.noreply.github.com>
2020-07-31 10:59:05 -0700

    Update pre_pokemon.py

--- pre_pokemon.py
+++ pre_pokemon.py
@@ -36,54 +36,44 @@ class TrainerEncoder(JSONEncoder):
         return o.__dict__
 
 
+# a file is created that allows you to write over a JSON file
 out_file = open('pokemon.json', 'w')
 
-
-add_trainer = input("Do you want to add a trainer? (yes/no) ")
-if add_trainer == 'no':
-    quit()
-else:
-    input_trainer_first_name = input("What is your first name? ")
-    input_trainer_last_name = input("What is your last name? ")
-    input_trainer_age = input("What is your age? ")
-    input_trainer_ethnicity = input("What is your ethnicity? ")
-    _trainer_pokemon = []
-    trainer = Trainer(input_trainer_first_name, input_trainer_first_name, input_trainer_age, input_trainer_ethnicity,
-                      _trainer_pokemon)
-    print(TrainerEncoder().encode(trainer))
-#    trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
-#    json.dump(trainer, out_file, cls=TrainerEncoder)
-    print()
-
+# two lists are used that allows the storage of created trainers and Pokemon
+_trainers = []
 _trainer_pokemon = []
 while True:
-    add_pokemon = input("Do you want to add a Pokemon? (yes/no) ")
-    if add_pokemon == 'no':
-        add_trainer = input("Do you want to add a trainer? (yes/no) ")
-        if add_trainer == 'no':
+    # Ask for trainer information
+    add_trainer = input("Do you want to add a trainer? (yes/no) ")
+    if add_trainer == 'no':
+        break
+    else:
+        input_trainer_first_name = input("What is your first name? ")
+        input_trainer_last_name = input("What is your last name? ")
+        input_trainer_age = input("What is your age? ")
+        input_trainer_ethnicity = input("What is your ethnicity? ")
+
+# this list is restarted and then now used to collect Pokemon information
+    _trainer_pokemon = []
+#
+    while True:
+        add_pokemon = input("Do you want to add a Pokemon? (yes/no) ")
+        if add_pokemon == 'no':
             break
         else:
-            input_trainer_first_name = input("What is your first name? ")
-            input_trainer_last_name = input("What is your last name? ")
-            input_trainer_age = input("What is your age? ")
-            input_trainer_ethnicity = input("What is your ethnicity? ")
-            _trainer_pokemon = []
-            trainer = Trainer(input_trainer_first_name, input_trainer_first_name, input_trainer_age,
-                              input_trainer_ethnicity, _trainer_pokemon)
-            print(TrainerEncoder().encode(trainer))
-            trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
-            json.dump(trainer, out_file, cls=TrainerEncoder)
-            print()
+            input_name = input("What is your Pokemon's name? ")
+            input_type = input("What is your Pokemon's type? ")
+            input_level = input("What is your Pokemon's level? ")
+            pokemon = Pokemon(input_name, input_type, input_level)
+            _trainer_pokemon.append(pokemon)
 
-    else:
-        input_name = input("What is your Pokemon's name? ")
-        input_type = input("What is your Pokemon's type? ")
-        input_level = input("What is your Pokemon's level? ")
-        pokemon = Pokemon(input_name, input_type, input_level)
-        print(PokemonEncoder().encode(pokemon))
-        trainer = Trainer(input_trainer_first_name, input_trainer_last_name, input_trainer_age, input_trainer_ethnicity,
-                          _trainer_pokemon)
-        _trainer_pokemon.append(pokemon)
-        print(TrainerEncoder().encode(trainer))
-trainerJsonStr = json.dumps(trainer, indent=6, cls=TrainerEncoder)
-json.dump(trainer, out_file, cls=TrainerEncoder)
+# all this information, trainer and Pokemon is kept as a class object
+    trainer = Trainer(input_trainer_first_name, input_trainer_last_name, input_trainer_age,
+                      input_trainer_ethnicity, _trainer_pokemon)
+# the multiple objects are then stored within the _trainers list
+    _trainers.append(trainer)
+
+
+# trainerJsonStr = json.dumps(_trainers, indent=6, cls=TrainerEncoder)
+# all the stored data is then dumped inside pokemon.JSON and sorted via key-value in alphabetical order
+json.dump(_trainers, out_file, indent=4, sort_keys=True, cls=TrainerEncoder)
